db7092d53bd7333ca5df9cc0ecae4d86e85eb269
Brandon Carson <brandoncarson@ou.edu>
2020-08-10 21:28:07 -0500

    removed unused code

--- unet/model.py
+++ unet/model.py
@@ -21,38 +21,10 @@ class double_conv(nn.Module):
         return x
 
 
-class double_conv3d(nn.Module):
-    '''(conv => BN => ReLU) * 2'''
-    def __init__(self, in_ch=1, out_ch=1):
-        super(double_conv3d, self).__init__()
-        self.conv = nn.Sequential(
-            nn.Conv3d(in_ch, out_ch, 3, padding=1),
-            nn.BatchNorm3d(out_ch),
-            nn.ReLU(inplace=True),
-            nn.Conv3d(out_ch, out_ch, 3, padding=1),
-            nn.BatchNorm3d(out_ch),
-            nn.ReLU(inplace=True)
-        )
-
-    def forward(self, x):
-        x = self.conv(x)
-        return x
-
-
 class inconv(nn.Module):
     def __init__(self, in_ch, out_ch):
         super(inconv, self).__init__()
         self.conv = double_conv(in_ch, out_ch)
-
-    def forward(self, x):
-        x = self.conv(x)
-        return x
-
-
-class inconv3d(nn.Module):
-    def __init__(self, in_ch, out_ch):
-        super(inconv3d, self).__init__()
-        self.conv = double_conv3d(in_ch, out_ch)
 
     def forward(self, x):
         x = self.conv(x)
@@ -70,44 +42,7 @@ class down(nn.Module):
     def forward(self, x):
         x = self.mpconv(x)
         return x
- 
 
-class down3d(nn.Module):
-    def __init__(self, in_ch, out_ch):
-        super(down3d, self).__init__()
-        self.mpconv = nn.Sequential(
-            nn.MaxPool3d(2),
-            double_conv3d(in_ch, out_ch)
-        )
-
-    def forward(self, x):
-        x = self.mpconv(x)
-        return x
-
-
-class up3d(nn.Module):
-    def __init__(self, in_ch, out_ch, bilinear=False):
-        super(up3d, self).__init__()
-
-        #  would be a nice idea if the upsampling could be learned too,
-        #  but my machine do not have enough memory to handle all those weights
-        if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-        else:
-            self.up = nn.ConvTranspose3d(in_ch//2, in_ch//2, 2, stride=2)
-
-        self.conv = double_conv3d(in_ch, out_ch)
-
-    def forward(self, x1, x2):
-        x1 = self.up(x1)
-        diffX = x1.size()[2] - x2.size()[2]
-        diffY = x1.size()[3] - x2.size()[3]
-        x2 = F.pad(x2, (diffX // 2, int(diffX / 2),
-                        diffY // 2, int(diffY / 2)))
-        x = torch.cat([x2, x1], dim=1)
-        x = self.conv(x)
-        return x
-   
 
 class up(nn.Module):
     def __init__(self, in_ch, out_ch, bilinear=True):
@@ -143,15 +78,6 @@ class outconv(nn.Module):
         return x
 
 
-class outconv3d(nn.Module):
-    def __init__(self, in_ch, out_ch):
-        super(outconv3d, self).__init__()
-        self.conv = nn.Conv3d(in_ch, out_ch, 1)
-
-    def forward(self, x):
-        x = self.conv(x)
-        return x
-
 class UNet(nn.Module):
     def __init__(self, n_channels, n_classes, large_model=False):
         super(UNet, self).__init__()
@@ -177,35 +103,6 @@ class UNet(nn.Module):
             self.up3 = up(128, 32)
             self.up4 = up(64, 32)
             self.outc = outconv(32, n_classes)
-
-    def forward(self, x):
-        x1 = self.inc(x)
-        x2 = self.down1(x1)
-        x3 = self.down2(x2)
-        x4 = self.down3(x3)
-        x5 = self.down4(x4)
-        x = self.up1(x5, x4)
-        x = self.up2(x, x3)
-        x = self.up3(x, x2)
-        x = self.up4(x, x1)
-        x = self.outc(x)
-        return x
-
-
-class UNet3d(nn.Module):
-    def __init__(self, n_channels, n_classes, large_model=False):
-        super(UNet3d, self).__init__()
-
-        self.inc = inconv3d(n_channels, 32)
-        self.down1 = down3d(32, 64)
-        self.down2 = down3d(64, 128)
-        self.down3 = down3d(128, 256)
-        self.down4 = down3d(256, 256)
-        self.up1 = up3d(512, 128)
-        self.up2 = up3d(256, 64)
-        self.up3 = up3d(128, 32)
-        self.up4 = up3d(64, 32)
-        self.outc = outconv(32, n_classes)
 
     def forward(self, x):
         x1 = self.inc(x)
