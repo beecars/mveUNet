9bf5804c0aa585b56c0a5282c81eec441b91f6fd
Brandon Carson <brandoncarson@ou.edu>
2020-08-15 15:54:03 -0500

    removed redundant imgproc defs from Dataset class

--- unet/dataset.py
+++ unet/dataset.py
@@ -1,11 +1,14 @@
-from torch.utils.data import Dataset, DataLoader
-from PIL import Image
 import numpy as np
 import random
 
+import torch
+from torch.utils.data import Dataset
+
+from utils import readBinImage, readUCharImage
+
 class CTMaskDataset(Dataset):
     def __init__(self, 
-                 ct_mask_data, 
+                 ct_mask_data,
                  img_size=(512, 512), 
                  offset=(150,150), 
                  output_size=(320, 320), 
@@ -16,35 +19,11 @@ class CTMaskDataset(Dataset):
         self.offset = offset
         self.output_size = output_size
         self.augment = augment
-  
-    def _readUCharImage(self, fname):
-        with open(fname, 'rb') as f:
-            rawData = f.read()
-            img = Image.frombytes('L', self.img_size, rawData)
-            return np.array(img).astype(np.float32)
- 
-    def _readBinImage(self, fname):
-        with open(fname, 'rb') as f:
-            rawData = f.read()
-            img = Image.frombytes('I', self.img_size, rawData)
-            return np.array(img).T.astype(np.float32)
-        
-    def _readUCharImageToPIL(self, fname):
-        with open(fname, 'rb') as f:
-            rawData = f.read()
-            img = Image.frombytes('L', self.img_size, rawData)
-            return img
- 
-    def _readBinImageToPIL(self, fname):
-        with open(fname, 'rb') as f:
-            rawData = f.read()
-            img = Image.frombytes('I', self.img_size, rawData)
-            return img
             
     def __getitem__(self, idx):
         ct_fname, mask_fname = self.data[idx]
-        ct = self._readBinImage(ct_fname)
-        mask = self._readUCharImage(mask_fname)
+        ct = readBinImage(ct_fname)
+        mask = readUCharImage(mask_fname)
         Kx, Ky = self.offset
         Lx, Ly = self.output_size
         if self.augment:
@@ -58,9 +37,12 @@ class CTMaskDataset(Dataset):
         if self.augment and np.random.randn() > 0.5:
             ct = np.fliplr(ct).copy()
             mask = np.fliplr(mask).copy()
+
+        ct = torch.from_numpy(ct).unsqueeze(0).float()
+        mask = torch.from_numpy(mask).unsqueeze(0).float()
         
-        return {'data': np.expand_dims(ct, axis=0), 
-                'label': np.expand_dims(mask, axis=0)}
+        return {'data': ct, 
+                'label': mask}
     
     def __len__(self):
         return len(self.data)
