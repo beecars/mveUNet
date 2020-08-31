c9f71573d0e5c5024186c702109dc2f728d451f2
Brandon Carson <brandoncarson@ou.edu>
2020-08-31 00:39:11 -0500

    working on multiclass loss (WIP)

--- unet/losses.py
+++ unet/losses.py
@@ -27,18 +27,6 @@ class FocalLoss(nn.Module):
             return torch.mean(F_loss)
         else:
             return F_loss
-        
-
-class MixedLoss(nn.Module):
-    def __init__(self, alpha, gamma):
-        super().__init__()
-        self.alpha = alpha
-        self.focal = FocalLoss(4.0, gamma, logits=True, reduce=False)
-        
-    def forward(self, input, target):
-        loss = self.alpha*self.focal(input, target, ) - torch.log(dice_loss(input, target))
-        loss = self.focal(input, target)
-        return loss.mean()
 
 
 def dice_loss(input, target):
@@ -52,6 +40,18 @@ def dice_loss(input, target):
     return ((2.0 * intersection + smooth) / (iflat.sum() + tflat.sum() + smooth))
 
 
+class MixedLoss(nn.Module):
+    def __init__(self, alpha, gamma):
+        super().__init__()
+        self.alpha = alpha
+        self.focal = FocalLoss(4.0, gamma, logits=True, reduce=False)
+        
+    def forward(self, input, target):
+        loss = self.alpha*self.focal(input, target, ) - torch.log(dice_loss(input, target))
+        loss = self.focal(input, target)
+        return loss.mean()
+
+
 def dice(pred, targs):
     pred = (pred>0).float()
     return 2.0 * (pred*targs).sum() / ((pred+targs).sum() + 1.0)
@@ -61,39 +61,3 @@ def IoU(pred, targs):
     pred = (pred>0).float()
     intersection = (pred*targs).sum()
     return intersection / ((pred+targs).sum() - intersection + 1.0)
-
-
-def dice_loss_multi(output, target, weights=None, ignore_index=None):
-    """
-    output : NxCxHxW Variable
-    target :  NxHxW LongTensor
-    weights : C FloatTensor
-    ignore_index : int index to ignore from loss
-    """
-    eps = 0.0001
-
-    output = output.exp()
-    encoded_target = output.detach() * 0
-    if ignore_index is not None:
-        mask = target == ignore_index
-        target = target.clone()
-        target[mask] = 0
-        encoded_target.scatter_(1, target.unsqueeze(1), 1)
-        mask = mask.unsqueeze(1).expand_as(encoded_target)
-        encoded_target[mask] = 0
-    else:
-        encoded_target.scatter_(1, target.unsqueeze(1), 1)
-
-    if weights is None:
-        weights = 1
-
-    intersection = output * encoded_target
-    numerator = 2 * intersection.sum(0).sum(1).sum(1)
-    denominator = output + encoded_target
-
-    if ignore_index is not None:
-        denominator[mask] = 0
-    denominator = denominator.sum(0).sum(1).sum(1) + eps
-    loss_per_channel = weights * (1 - (numerator / denominator))
-
-    return loss_per_channel.sum() / output.size(1)
