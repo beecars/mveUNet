72eed06b2a092b119b9096afd0587b556d95ba49
Brandon Carson <brandoncarson@ou.edu>
2020-09-11 19:37:37 -0500

    removed mixed loss - no difference in perf

--- unet-milesial/dice_loss.py
+++ unet-milesial/dice_loss.py
@@ -4,59 +4,12 @@ import torch.nn.functional as F
 from torch.autograd import Function
 
 ################################################################################
-##### CHUONG VALIDATION IOU: added to eval_net in eval.py
+##### VALIDATION IOU: added to eval_net in eval.py
 ################################################################################
 def IoU(pred, targs):
     pred = (pred>0).float()
     intersection = (pred*targs).sum()
     return intersection / ((pred+targs).sum() - intersection + 1.0)
-################################################################################
-##### CHUONG MIXED LOSS
-################################################################################
-class FocalLoss(nn.Module):
-    def __init__(self, alpha=1, gamma=2, logits=False, reduce=True):
-        super(FocalLoss, self).__init__()
-        self.alpha = alpha
-        self.gamma = gamma
-        self.logits = logits
-        self.reduce = reduce
-
-    def forward(self, inputs, targets):
-        if self.logits:
-            BCE_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduce=False)
-        else:
-            BCE_loss = F.binary_cross_entropy(inputs, targets, reduce=False)
-        pt = torch.exp(-BCE_loss)
-        F_loss = self.alpha * (1-pt)**self.gamma * BCE_loss
-
-        if self.reduce:
-            return torch.mean(F_loss)
-        else:
-            return F_loss
-
-
-def dice_loss(input, target):
-    input = torch.sigmoid(input)
-    smooth = 1.0
-
-    iflat = input.view(-1)
-    tflat = target.view(-1)
-    intersection = (iflat * tflat).sum()
-    
-    return ((2.0 * intersection + smooth) / (iflat.sum() + tflat.sum() + smooth))
-
-
-class MixedLoss(nn.Module):
-    def __init__(self, alpha, gamma):
-        super().__init__()
-        self.alpha = alpha
-        self.focal = FocalLoss(4.0, gamma, logits=True, reduce=False)
-        
-    def forward(self, input, target):
-        loss = self.alpha*self.focal(input, target, ) - torch.log(dice_loss(input, target))
-        loss = self.focal(input, target)
-        return loss.mean()
-################################################################################
 
 
 class DiceCoeff(Function):
