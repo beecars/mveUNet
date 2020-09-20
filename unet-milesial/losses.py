f863daeb306326fbb94bf7d4afa5937dfed5ad71
Brandon Carson <brandoncarson@ou.edu>
2020-09-20 12:24:39 -0500

    formatting

--- unet-milesial/losses.py
+++ unet-milesial/losses.py
@@ -60,7 +60,7 @@ class DiceCoeff(Function):
 
 class FocalLoss(nn.Module):
     """Focal loss (from Chuong's code)"""
-    def __init__(self, alpha=1, gamma=2, logits=False, reduce=True):
+    def __init__(self, alpha=1, gamma=2, logits=True, reduce=True):
         super(FocalLoss, self).__init__()
         self.alpha = alpha
         self.gamma = gamma
@@ -69,9 +69,13 @@ class FocalLoss(nn.Module):
 
     def forward(self, inputs, targets):
         if self.logits:
-            BCE_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduce=False)
+            BCE_loss = F.binary_cross_entropy_with_logits(inputs, 
+                                                          targets, 
+                                                          reduction = 'none')
         else:
-            BCE_loss = F.binary_cross_entropy(inputs, targets, reduce=False)
+            BCE_loss = F.binary_cross_entropy(inputs, 
+                                              targets, 
+                                              reduction = 'none')
         
         pt = torch.exp(-BCE_loss)
         F_loss = self.alpha * (1-pt)**self.gamma * BCE_loss
