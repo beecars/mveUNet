dd5bfba14ed4308f4473ce0f59a307df28c31aa5
Brandon Carson <brandoncarson@ou.edu>
2020-09-20 22:45:31 -0500

    overhauled predict.py to predict CT volumes

--- unet-milesial/predict.py
+++ unet-milesial/predict.py
@@ -6,131 +6,104 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 from PIL import Image
+from torch.utils import data
 from torchvision import transforms
+from tqdm.std import tqdm
 
 from unet import UNet
-from utils.data_vis import plot_img_and_mask
-from utils.dataset import BasicDataset
+from utils.dataset import CTVolumeDataset
+from utils.utils import matchFilesFromPatient
 
 
-def predict_img(net,
-                full_img,
-                device,
-                scale_factor = 1,
-                out_threshold = 0.5):
+def predict_volume(net,
+                   device,
+                   ct_data,
+                   p_threshold = 0.5):
     net.eval()
 
-    img = torch.from_numpy(BasicDataset.preprocess(full_img, scale_factor))
+    volume = CTVolumeDataset(ct_data)
+    n_cts = len(volume)
+    img_shape = volume[0]['image'].size()[1:3]
+    probs_volume = torch.empty(n_cts, img_shape[0], img_shape[1])
+    
+    with tqdm(total = n_cts, 
+                  desc = f'Predicting Volume', 
+                  unit = 'scans',
+                  ascii = True,
+                  leave = False,
+                  bar_format = '{l_bar}{bar:30}{r_bar}{bar:-10b}') as pbar:
 
-    img = img.unsqueeze(0)
-    img = img.to(device=device, dtype=torch.float32)
+        with torch.no_grad():
+            for i, scan in enumerate(volume):
+                
+                ct = scan['image'].unsqueeze(0)
+                ct = ct.to(device)
 
-    with torch.no_grad():
-        output = net(img)
+                output = net(ct)
+                output = torch.squeeze(output)
 
-        if net.n_classes > 1:
-            probs = F.softmax(output, dim=1)
-        else:
-            probs = torch.sigmoid(output)
+                if net.n_classes > 1:
+                    probs = F.softmax(output, dim = 1)
+                else:
+                    probs = torch.sigmoid(output)
 
-        probs = probs.squeeze(0)
+                probs_volume[i] = probs
+                
+                pbar.update()
 
-        tf = transforms.Compose(
-            [
-                transforms.ToPILImage(),
-                transforms.Resize(full_img.size[1]),
-                transforms.ToTensor()
-            ]
-        )
+            mask_volume = probs_volume > p_threshold
 
-        probs = tf(probs.cpu())
-        full_mask = probs.squeeze().cpu().numpy()
-
-    return full_mask > out_threshold
+    return mask_volume.numpy().astype(int)
 
 
 def get_args():
-    parser = argparse.ArgumentParser(description='Predict masks from input images',
+    parser = argparse.ArgumentParser(description='Predict mask volume from input volume',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--model', '-m', default='MODEL.pth',
-                        metavar='FILE',
-                        help="Specify the file in which the model is stored")
-    parser.add_argument('--input', '-i', metavar='INPUT', nargs='+',
-                        help='filenames of input images', required=True)
-
-    parser.add_argument('--output', '-o', metavar='INPUT', nargs='+',
-                        help='Filenames of ouput images')
-    parser.add_argument('--viz', '-v', action='store_true',
-                        help="Visualize the images as they are processed",
-                        default=False)
-    parser.add_argument('--no-save', '-n', action='store_true',
-                        help="Do not save the output masks",
-                        default=False)
-    parser.add_argument('--mask-threshold', '-t', type=float,
+    parser.add_argument('--patient', '-p', metavar='INPUT', type=int,
+                        help="Specify the patient index.", required = True)
+    parser.add_argument('--day', '-d', metavar='INPUT', type=int,
+                        help="Specify the day index.", required=True)
+    parser.add_argument('--classes', '-cl', metavar='INPUT', default=1,
+                        help="Specify the number of classes.", required=False)
+    parser.add_argument('--channels', '-ch', metavar='INPUT', default=1,
+                        help="Specify the number of channels.", required=False)
+    parser.add_argument('--state_dict', '-s', metavar='INPUT', required=True,
+                        help="Filename of state dict (.pth)")
+    parser.add_argument('--filename', '-f', metavar='INPUT', required=False,
+                        help="Filename of ouput file (.npy)", default='volume.npy')
+    parser.add_argument('--mask-threshold', '-t', metavar = 'INPUT', type=float,
                         help="Minimum probability value to consider a mask pixel white",
                         default=0.5)
-    parser.add_argument('--scale', '-s', type=float,
-                        help="Scale factor for the input images",
-                        default=1)
-
     return parser.parse_args()
 
 
-def get_output_filenames(args):
-    in_files = args.input
-    out_files = []
-
-    if not args.output:
-        for f in in_files:
-            pathsplit = os.path.splitext(f)
-            out_files.append("{}_OUT{}".format(pathsplit[0], pathsplit[1]))
-    elif len(in_files) != len(args.output):
-        logging.error("Input files and output files are not of the same length")
-        raise SystemExit()
-    else:
-        out_files = args.output
-
-    return out_files
-
-
-def mask_to_image(mask):
-    return Image.fromarray((mask * 255).astype(np.uint8))
-
-
 if __name__ == "__main__":
+    ############################################################################
+    ### SET PREDICTION VOLUME DIRECTORY
+    dir_save = 'unet-milesial/.predictions/'
+    ############################################################################
+    try:
+        os.makedirs(dir_save)
+    except OSError:
+        pass
+
     args = get_args()
-    in_files = args.input
-    out_files = get_output_filenames(args)
+    logging.basicConfig(level=logging.INFO,
+                        format="[%(levelname)s] %(message)s")
 
-    net = UNet(n_channels=3, n_classes=1)
+    ct_data = matchFilesFromPatient(args.patient, args.day, mode = 'CT_ONLY')
+    
+    net = UNet(args.channels, args.classes, bilinear = False).cuda()
+    state = torch.load(args.state_dict)
+    net.load_state_dict(state)
+    device = torch.device('cuda')
 
-    logging.info("Loading model {}".format(args.model))
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    logging.info(f"State dict {args.state_dict} loaded to UNet model")
+    logging.info(f"Predicting {args.classes} classes from {args.channels} channels")
     logging.info(f'Using device {device}')
-    net.to(device=device)
-    net.load_state_dict(torch.load(args.model, map_location=device))
+    logging.info("Starting volume prediction...")
 
-    logging.info("Model loaded !")
+    volume = predict_volume(net, device, ct_data)
+    np.save(dir_save + args.filename, volume)
 
-    for i, fn in enumerate(in_files):
-        logging.info("\nPredicting image {} ...".format(fn))
-
-        img = Image.open(fn)
-
-        mask = predict_img(net=net,
-                           full_img=img,
-                           scale_factor=args.scale,
-                           out_threshold=args.mask_threshold,
-                           device=device)
-
-        if not args.no_save:
-            out_fn = out_files[i]
-            result = mask_to_image(mask)
-            result.save(out_files[i])
-
-            logging.info("Mask saved to {}".format(out_files[i]))
-
-        if args.viz:
-            logging.info("Visualizing results for image {}, close to continue ...".format(fn))
-            plot_img_and_mask(img, mask)
+    logging.info(f"Prediction saved to {dir_save}{args.filename}")
