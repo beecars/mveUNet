aeee9c8678878fd4fc1e7d1883be64f4ab81cff1
Brandon Carson <brandoncarson@ou.edu>
2020-09-27 19:28:19 -0500

    evaluates on volumes now instead of scans

--- unet-2D/eval.py
+++ unet-2D/eval.py
@@ -1,39 +1,89 @@
+import numpy as np
 import torch
 import torch.nn.functional as F
-from tqdm import tqdm
+from tqdm.std import tqdm
 
+from predict import predict_vol_from_seq
+from utils.dataset import readUCharImage
 from losses import dice_coeff, iou
 
-def eval_net(net, loader, device):
-    '''Evaluation without the densecrf with the dice coefficient'''
-    net.eval()
-    mask_type = torch.float32 if net.n_classes == 1 else torch.long
-    n_val = len(loader)  # the number of batch
-    dice_tot = 0
-    iou_tot = 0
-    with tqdm(total=n_val, 
-              desc='Validation', 
-              unit='batch', 
-              leave=False,
-              ascii = True,
-              bar_format='{l_bar}{bar:30}{r_bar}{bar:-10b}') as pbar:
-              
-        for batch in loader:
-            imgs, true_masks = batch['image'], batch['target']
-            imgs = imgs.to(device=device, dtype=torch.float32)
-            true_masks = true_masks.to(device=device, dtype=mask_type)
 
-            with torch.no_grad():
-                mask_pred = net(imgs)
+def eval_volume(net,
+                device,
+                ct_mask_data,
+                p_threshold = 0.5):
+    '''
+    From a sequence of CT images, predicts a segmented "volume" and compares
+    it with the ground truth. Returns <metrics>.
+    @params:
+        ct_mask_data = a specific data list generated for the REVEAL CT data
+                        by using the matchFilesFromPatients() function. 
+        p_threshold = the value above which a pixel is classified as true.
+    @returns: 
+        a tuple of (dice, iou) metrics.
+    '''
+    ct_mask_data = np.array(ct_mask_data)
+    ct_data = ct_mask_data[:,0]
+    mask_data = ct_mask_data[:,1]
 
-            if net.n_classes > 1:
-                dice_tot += F.cross_entropy(mask_pred, true_masks).item()
-            else:
-                pred = torch.sigmoid(mask_pred)
-                pred = (pred > 0.5).float()
-                dice_tot += dice_coeff(pred, true_masks).item()
-                iou_tot += iou(pred, true_masks).item()
-            pbar.update()
+    # create prediction volume
+    pred_volume = predict_vol_from_seq(net,
+                                       device,
+                                       ct_data,
+                                       p_threshold = p_threshold)
+    
+    # create ground-truth volume
+    mask_shape = readUCharImage(mask_data[0]).shape
+    test_volume = np.zeros((mask_shape[0], mask_shape[1], len(mask_data)))
+    for i, file in enumerate(mask_data):
+        test_volume[:, :, i] = readUCharImage(file)
+    test_volume = test_volume.astype(int)
 
-    net.train()
-    return (dice_tot / n_val), (iou_tot / n_val)
+    # calculate intersection over union between volumes
+    intersection = np.logical_and(pred_volume, test_volume).astype(int)
+    union = np.logical_or(pred_volume, test_volume).astype(int)
+    iou = np.sum(intersection)/np.sum(union)
+
+    # calculate dice coefficient between volumes
+    a_volume = np.sum(pred_volume)
+    b_volume = np.sum(test_volume)
+    dice = (2 * np.sum(intersection)) / (a_volume + b_volume)
+
+    return dice, iou
+
+def eval_volumes(net,
+                 device,
+                 ct_mask_data,
+                 volume_idxs,
+                 p_threshold = 0.5):
+    '''
+    From an ordered list representing sequences of CT images of more than one
+    CT volume, computes metrics over the individual volumes represented
+    in the input data list and returns the average of those metrics.
+    @params:
+        ct_mask_data = a specific data list generated for the REVEAL CT data
+                        by using the matchFilesFromPatients() function. 
+        volume_idx = a specific list of (patient, day) tuples generated for 
+                        REVEAL CT data by using the matchFilesFromPatients() 
+                        function.
+        p_threshold = the value above which a pixel is classified as true.
+    @returns: 
+        a tuple of (dice, iou) metrics.
+    '''
+    dice_sum = 0
+    iou_sum = 0
+    for volume_idx in volume_idxs:
+        temp_data = []
+        for line in ct_mask_data:                                   # build
+            if ('P_' + '{0:0=2d}'.format(volume_idx[0]) in line[0]  # individual
+                and f'Day_{volume_idx[1]}' in line[0]):             # volume
+                temp_data.append(line)                              # subset
+            
+        dice, iou = eval_volume(net,
+                                device,
+                                temp_data,
+                                p_threshold = p_threshold)
+        dice_sum = dice_sum + dice
+        iou_sum = iou_sum + iou
+
+    return dice_sum/len(volume_idxs), iou_sum/len(volume_idxs)
