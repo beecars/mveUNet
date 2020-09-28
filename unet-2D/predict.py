62f777e3d11835157cb4641be5dfe9e12de6007a
Brandon Carson <brandoncarson@ou.edu>
2020-09-27 19:29:01 -0500

    added predict_vol_from_vol()

--- unet-2D/predict.py
+++ unet-2D/predict.py
@@ -3,14 +3,13 @@ import logging
 import os
 
 import numpy as np
+from scipy.io import loadmat
 import torch
 import torch.nn.functional as F
-from torch.utils import data
-from torchvision import transforms
 from tqdm.std import tqdm
 
 from unet import UNet
-from utils.dataset import CTVolumeDataset
+from utils.dataset import CTSequenceDataset, CTVolumeDataset
 from utils.utils import matchFilesFromPatient
 
 
@@ -19,12 +18,23 @@ def predict_vol_from_seq(net,
                          ct_data,
                          threshold = True,
                          p_threshold = 0.5):
-    ''' This function takes a sequence (list) of CT scans, runs them through
-    a UNet model, and assembles and returns a prediction "volume" data object.
+    ''' 
+    Takes a sequence (list) of CT scans, runs them through a CNN model, and 
+    assembles and returns a prediction "volume" data object.
+    @params:
+        ct_data = a specific data list generated for the REVEAL CT data
+                  by using the matchFilesFromPatients() function. 
+        threshold = boolean, if False volume will be returned as float 
+                    prediction values. If true, volume will be thresholded and
+                    returned as a binary prediction mask.
+        p_threshold = the threshold value above which a pixel is classified as 
+                      true.
+    @returns:
+        a binary prediction volume or a probabily prediction volume as a nparray
     '''
     net.eval()
 
-    volume = CTVolumeDataset(ct_data)
+    volume = CTSequenceDataset(ct_data)
     n_cts = len(volume)
     img_shape = volume[0]['image'].size()[1:3]
     probs_volume = torch.empty(img_shape[0], img_shape[1], n_cts)
@@ -61,6 +71,62 @@ def predict_vol_from_seq(net,
     
     return probs_volume.numpy().astype(float)
     
+def predict_vol_from_vol(net,
+                         device,
+                         x,
+                         mode = 'data',
+                         threshold = True,
+                         p_threshold = 0.5):
+    '''
+    Predicts a CT volume from a volume data source of one of the following: 
+    a 3D numpy data array, a MATLAB .mat file, or a numpy .npy file.
+    @params:
+        x = either a 3D np array data OR a filepath, depending on mode.
+        mode = 'npy', 'mat', or 'data' (default is data)
+        threshold = boolean, if False volume will be returned as float 
+                    prediction values. If true, volume will be thresholded and
+                    returned as a binary prediction mask.
+        p_threshold = the threshold value above which a pixel is classified as 
+                      true.
+    @returns:
+        a binary prediction volume or a probabily prediction volume as a nparray
+    '''
+    volume = CTVolumeDataset(x, mode)
+    n_cts = len(volume)
+    img_shape = volume[0].shape
+    probs_volume = torch.empty(img_shape[1], img_shape[2], n_cts)
+    
+    with tqdm(total = n_cts, 
+                  desc = f'Predicting Volume', 
+                  unit = 'scans',
+                  ascii = True,
+                  leave = False,
+                  bar_format = '{l_bar}{bar:30}{r_bar}{bar:-10b}') as pbar:
+
+        with torch.no_grad():
+            for i, scan in enumerate(volume):
+                
+                ct = scan.unsqueeze(0)
+                ct = ct.to(device)
+
+                output = net(ct)
+                output = torch.squeeze(output)
+
+                if net.n_classes > 1:
+                    probs = F.softmax(output, dim = 1)
+                else:
+                    probs = torch.sigmoid(output)
+
+                probs_volume[:,:, i] = probs
+                
+                pbar.update()
+
+            if threshold:
+                probs_volume = probs_volume > p_threshold
+            else:
+                probs_volume = probs_volume
+    
+    return probs_volume.numpy().astype(float)
 
 
 def get_args():
@@ -86,7 +152,7 @@ def get_args():
 
 if __name__ == "__main__":
     ############################################################################
-    ### SET PREDICTION VOLUME DIRECTORY
+    ### SET PREDICTION VOLUME SAVE DIRECTORY
     dir_save = 'unet-2D/.predictions/'
     ############################################################################
     try:
