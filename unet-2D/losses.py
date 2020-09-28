ae9da1057ca0fe71560a167b90a3752ae90d6149
Brandon Carson <brandoncarson@ou.edu>
2020-09-27 19:28:37 -0500

    removed unused losses

--- unet-2D/losses.py
+++ unet-2D/losses.py
@@ -3,38 +3,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Function
 
-def dice_coeff(preds, targets):
-    """Dice coeff for batches. Pretty much just calls DiceCoeff.forward() a 
-       bunch of times, sums, and averages."""
-    if preds.is_cuda:
-        s = torch.FloatTensor(1).cuda().zero_()
-    else:
-        s = torch.FloatTensor(1).zero_()
-    
-    i = 0
-    for i, c in enumerate(zip(preds, targets)):
-        s = s + DiceCoeff().forward(c[0], c[1])
-
-    return s / (i + 1)
-
-def iou(preds, targets):
-    """Intersection over union for evaluation. Expects batches."""
-    smooth = 1e-6
-    preds = preds.squeeze(1).int()  # BATCH x 1 x H x W => BATCH x H x W
-    targets = targets.squeeze(1).int()
-    
-    intersection = (preds & targets).float().sum((1, 2))
-    union = (preds | targets).float().sum((1, 2))    
-    
-    iou = (intersection + smooth) / (union + smooth)  # smooth avoids 0/0
-
-    return iou.mean()
-
 
 class DiceCoeff(Function):
-    """Dice coefficient. Can be used as loss since it has a .backward() method
-       but BCE has been better for training on the CT data. Method .forward()
-       useful as a validation metric."""
+    '''
+    Dice coefficient. Can be used as loss since it has a .backward() method
+    but BCE has been better for training on the CT data. Method .forward()
+    useful as a validation metric.
+    '''
     def forward(self, input, target):
         self.save_for_backward(input, target)
         eps = 0.0001
@@ -60,7 +35,9 @@ class DiceCoeff(Function):
 
 
 class FocalLoss(nn.Module):
-    """Focal loss (from Chuong's code)"""
+    '''
+    Focal loss (from Chuong's code)
+    '''
     def __init__(self, alpha=1, gamma=2, logits=True, reduce=True):
         super(FocalLoss, self).__init__()
         self.alpha = alpha
@@ -85,16 +62,3 @@ class FocalLoss(nn.Module):
             return torch.mean(F_loss)
         else:
             return F_loss
-
-class MixedLoss(nn.Module):
-    """Mixed loss (from Chuong's code). Combination of Focal and Dice losses."""
-    def __init__(self, alpha, gamma):
-        super().__init__()
-        self.alpha = alpha
-        self.focal = FocalLoss(4.0, gamma, logits=True, reduce=False)
-        
-    def forward(self, input, target):
-        loss = self.alpha*self.focal(input, target, ) - torch.log(dice_coeff(input, target))
-        loss = self.focal(input, target)
-        return loss.mean()
-
