81b7a79e4eefd386f8413debb54b66e673b25520
Brandon Carson <brandoncarson@ou.edu>
2020-10-09 12:17:04 -0500

    added augmentation

---
+++ unet-2D/utils/augment.py
@@ -0,0 +1,23 @@
+import numpy as np
+import albumentations as albu
+
+
+def augment_dict(image_dict):
+
+    augmentation = albu.Compose([
+        albu.ShiftScaleRotate(shift_limit=0.2, rotate_limit=5, p=1),
+        albu.CenterCrop(320, 320),
+        albu.Flip(p=0.5).apply(1)
+        ])
+
+    image = np.expand_dims(image_dict['ct'], axis=2)
+    mask =  np.expand_dims(image_dict['spine'], axis=2)
+    
+    aug_imgs = augmentation(image = image, mask = mask)
+    for item in aug_imgs:
+        aug_imgs[item] = np.squeeze(aug_imgs[item])
+    
+    aug_imgs['ct'] = aug_imgs.pop('image')
+    aug_imgs['spine'] = aug_imgs.pop('mask')
+
+    return aug_imgs
