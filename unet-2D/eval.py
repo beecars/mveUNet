54316cb29d70b3d28e54f6dec421977dc0169f93
Brandon Carson <brandoncarson@ou.edu>
2020-10-30 15:37:18 -0500

    added jaccard, sensitivity, specificity

--- unet-2D/eval.py
+++ unet-2D/eval.py
@@ -5,6 +5,48 @@ import torch.nn.functional as F
 from predict import predict_vol_from_vol
 from utils.utils import loadMatData
 
+def jaccard(prediction_vol, truth_vol):
+    """Calculate jaccard/intersection-over-union between mask volumes."""
+    intersection = np.logical_and(prediction_vol, truth_vol).astype(int)
+    union = np.logical_or(prediction_vol, truth_vol).astype(int)
+    iou = np.sum(intersection)/np.sum(union)
+    return iou
+
+def dice_coeff(prediction_vol, truth_vol):
+    """Calculate dice coefficient between mask volumes."""
+    intersection = np.logical_and(prediction_vol, truth_vol).astype(int)
+    a_volume = np.sum(prediction_vol)
+    b_volume = np.sum(truth_vol)
+    dice = (2 * np.sum(intersection)) / (a_volume + b_volume)
+    return dice
+
+def sensitivity(prediction_vol, truth_vol):
+    """Calculate the sensitivity between a prediction mask and ground truth.
+    a.k.a. True Positive Rate
+    a.k.a. Recall
+    """
+    true_positives = np.sum(np.logical_and(prediction_vol, truth_vol).astype(int))
+    truth_positives = np.sum(truth_vol.astype(int))
+    return true_positives/truth_positives
+
+def specificity(prediction_vol, truth_vol):
+    """Calculate the specificity between a prediction mask and ground truth.
+    a.k.a. True Negative Rate
+    a.k.a. Selectivity
+    """
+    inverse_truth = np.logical_not(truth_vol).astype(int)
+    true_negatives = np.sum(np.logical_and(prediction_vol, inverse_truth).astype(int))
+    truth_negatives = np.sum(inverse_truth)
+    return true_negatives/truth_negatives
+
+def get_metrics(prediction_vol, truth_vol):
+    metrics_dict = {}
+    metrics_dict['jaccard'] = jaccard(prediction_vol, truth_vol)
+    metrics_dict['dice_coeff'] = dice_coeff(prediction_vol, truth_vol)
+    metrics_dict['sensitivity'] = sensitivity(prediction_vol, truth_vol)
+    metrics_dict['specificity'] = specificity(prediction_vol, truth_vol)
+    return metrics_dict
+
 def eval_volume(net,
                 device,
                 vol_idx,
@@ -33,14 +75,10 @@ def eval_volume(net,
                                        p_threshold = p_threshold)
     
     # calculate intersection over union between volumes
-    intersection = np.logical_and(pred_volume, mask_vol).astype(int)
-    union = np.logical_or(pred_volume, mask_vol).astype(int)
-    iou = np.sum(intersection)/np.sum(union)
+    iou = jaccard(pred_volume, mask_vol)
 
     # calculate dice coefficient between volumes
-    a_volume = np.sum(pred_volume)
-    b_volume = np.sum(mask_vol)
-    dice = (2 * np.sum(intersection)) / (a_volume + b_volume)
+    dice = dice_coeff(pred_volume, mask_vol)
 
     return dice, iou
 
